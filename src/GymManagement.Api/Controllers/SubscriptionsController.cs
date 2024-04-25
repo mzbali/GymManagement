a4342e8859aed34fe27eead918996f32342894d4
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-04-25 19:02:02 +0100

    refactor: translate presentation to application layer and vice versa

--- src/GymManagement.Api/Controllers/SubscriptionsController.cs
+++ src/GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -3,6 +3,7 @@ using GymManagement.Application.Subscriptions.Queries.GetSubscription;
 using GymManagement.Contracts.Subscriptions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using DomainSubscriptionType = GymManagement.Domain.Subscriptions.SubscriptionType;
 
 namespace GymManagement.Api.Controllers;
 
@@ -20,24 +21,28 @@ public class SubscriptionsController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> CreateSubscription(CreateSubscriptionRequest request)
     {
-        var command = new CreateSubscriptionCommand(request.SubscriptionType.ToString(), request.AdminId);
+        if (!DomainSubscriptionType.TryFromName(request.SubscriptionType.ToString(), out var subscriptionType))
+        {
+            return Problem(statusCode: StatusCodes.Status400BadRequest, detail: "Invalid subscription type");
+        }
+        var command = new CreateSubscriptionCommand(subscriptionType, request.AdminId);
         var createSubscriptionResult = await _mediator.Send(command);
         return createSubscriptionResult.MatchFirst(
             subscription => Ok(new SubscriptionResponse(subscription.Id, request.SubscriptionType)),
             error => Problem()
         );
     }
-    
+
     [HttpGet("{subscriptionId:guid}")]
-    public async Task<IActionResult> GetSubscription([FromRoute]Guid subscriptionId)
+    public async Task<IActionResult> GetSubscription([FromRoute] Guid subscriptionId)
     {
         var query = new GetSubscriptionQuery(subscriptionId);
         var getSubscriptionsResult = await _mediator.Send(query);
         return getSubscriptionsResult.MatchFirst(
             subscription => Ok(
                 new SubscriptionResponse(
-                    subscription.Id, 
-                    Enum.Parse<SubscriptionType>(subscription.SubscriptionType)
+                    subscription.Id,
+                    Enum.Parse<SubscriptionType>(subscription.SubscriptionType.Name)
                     )),
             error => Problem()
         );
